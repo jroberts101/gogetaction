43c6146477226943fbc6188ea5f5854586dbf4b2
john <mymail01@redhotjam.com>
2025-04-25 09:27:20 +1000

    refactor: code structure for improved readability and maintainability

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.json': ['prettier --write'],
-  '*.{md,mdx}': ['prettier --write'],
+  // '*.{md,mdx}': ['prettier --write'],
   '*.{yml,yaml}': ['prettier --write'],
 };
